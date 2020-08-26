f74447961589e5ef9e1062e3892030ac2c24832f
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-26 18:07:20 -0500

    Refactored routes and removed all calls to API to prepare for replacing API with database

--- server/index.js
+++ server/index.js
@@ -1,150 +1,103 @@
-const express = require("express");
-const axios = require("axios");
-const path = require("path");
-const apiUrl = 'http://52.26.193.201:3000/';
-const prefix = '/qa';
+/* eslint-disable import/extensions */
+/* eslint-disable camelcase */
+const express = require('express');
 
-let app = express();
+const app = express();
+const path = require('path');
+const dummy = require('../dummyData.js');
+
+// const apiUrl = 'http://52.26.193.201:3000/';
+const port = 3000;
+const prefix = '/qa';
 
 app.use(express.static('public'));
 
-app.use(express.json());
-
+// Potentially scrapping the code below
+// ------------------------------------------------------------------------
+// app.use(express.json());
 // should fix CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+// ------------------------------------------------------------------------
 
-// should send bundle.js file
+// Send bundle.js file (THIS IS FOR PROXY USE ONLY)
 app.get('/qaModule', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/../public/dist/bundle.js'));
+  res.sendFile(path.resolve(`${__dirname}/../public/dist/bundle.js`));
 });
 
-app.get(prefix + '/questions', async (req, res) => {
-  let { qLimit, aLimit, product_id } = req.query;
-  let url = apiUrl + 'qa/' + product_id;
+app.get(`${prefix}/questions`, async (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { qLimit, aLimit, product_id } = req.query;
+  // Dummy data
+  const data = dummy.dummyQuestions;
+  // Dummy questions
+  const questions = data.results;
+  // A boolean from legacy code that is supposed to deal with whether there is more questions than
+  // what should be shown somehow? Scrap or keep?
+  const isMoreQuestions = false;
 
-  let allQuestions = [];
-  let page = 0;
-
-  // loops until the response result is empty
-  // assumes that no page has more than 20 results
-  do {
-    let { data: response } = await axios.get(url, { params: { page: ++page, count: 20 } });
-    if (response.results.length === 0) break;
-    allQuestions = allQuestions.concat(response.results);
-  } while (true)
-
-  // checks if there is more than qLimit questions
-  let isMoreQuestions = allQuestions.length > qLimit;
-
-  // limits amount of questions displayed
-  let questions = allQuestions.slice(0, qLimit);
-
-  // limits amount of answers per question displayed
-  for (let i = 0; i < questions.length; i++) {
-    let limitedAnswers = {};
-
-    let ids = Object.keys(questions[i].answers);
-    let j = 0;
-    while (Object.keys(limitedAnswers).length < aLimit && ids.length !== 0) {
-      limitedAnswers[ids[j]] = questions[i].answers[ids[j]];
-      j++;
-    }
-
-    let isMoreAnswers = aLimit < ids.length;
-    limitedAnswers.isMoreAnswers = isMoreAnswers;
-
-    questions[i].answers = limitedAnswers;
-  }
-
+  // Client currently expects questions and isMoreQuestions
   res.send({ questions, isMoreQuestions });
 });
 
-app.get(prefix + '/moreAnswers', (req, res) => {
-  let { question_id } = req.query;
-  let url = apiUrl + `qa/${question_id}/answers`;
+app.get(`${prefix}/moreAnswers`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { question_id } = req.query;
+  // Dummy data
+  const data = dummy.dummyAnswers;
+  // Dummy answers
+  const { answers } = data;
+  // A boolean from legacy code that is supposed to deal with whether there is more answers than
+  // what should be shown somehow? Scrap or keep?
+  const isMoreAnswers = false;
 
-  axios.get(url)
-    .then((response) => {
-      let answers = response.data.results;
-      let isMoreAnswers = false;
-      res.send({ answers, isMoreAnswers });
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+  res.send({ answers, isMoreAnswers });
 });
 
-app.put(prefix + '/answer/helpful', (req, res) => {
-  let answer_id = req.body.answer_id;
-  let url = apiUrl + `qa/answer/${answer_id}/helpful`;
-
-  axios.put(url)
-    .then(() => {
-      res.send();
-    })
+app.put(`${prefix}/answer/helpful`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { answer_id } = req.body;
+  res.sendStatus(204);
 });
 
-app.put(prefix + '/answer/report', (req, res) => {
-  let answer_id = req.body.answer_id;
-  let url = apiUrl + `qa/answer/${answer_id}/report`;
-
-  axios.put(url)
-    .then(() => {
-      res.send();
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+// ROUTE TO REPORT QUESTIONS
+app.put(`${prefix}/question/report`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { question_id } = req.body;
+  res.sendStatus(204);
 });
 
-app.put(prefix + '/question/helpful', (req, res) => {
-  let question_id = req.body.question_id;
-  let url = apiUrl + `qa/question/${question_id}/helpful`;
-
-  axios.put(url)
-    .then(() => {
-      res.send();
-    })
+// ROUTE TO REPORT ANSWERS
+app.put(`${prefix}/answer/report`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { answer_id } = req.body;
+  res.sendStatus(204);
 });
 
-app.put(prefix + '/question/report', (req, res) => {
-  let question_id = req.body.question_id;
-  let url = apiUrl + `qa/question/${question_id}/report`;
-
-  axios.put(url)
-    .then(() => {
-      res.send();
-    })
+// ROUTE TO VOTE QUESTIONS HELPFUL
+app.put(`${prefix}/question/helpful`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { question_id } = req.body;
+  res.sendStatus(204);
 });
 
-app.post(prefix + '/question/add', (req, res) => {
-  let { product_id, ...questionSub } = req.body;
-  let url = apiUrl + `qa/${product_id}`;
-
-  axios.post(url, questionSub)
-    .then((response) => {
-      res.send(response.data);
-    })
+// ROUTE TO ADD A NEW QUESTION
+app.post(`${prefix}/question/add`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { product_id, ...questionSub } = req.body;
+  res.sendStatus(201);
 });
 
-app.post(prefix + '/answer/add', (req, res) => {
-  let { question_id, ...answerSub } = req.body;
-  let url = apiUrl + `qa/${question_id}/answers`;
-
-  axios.post(url, answerSub)
-    .then((response) => {
-      res.send();
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+// ROUTE TO ADD AN ANSWER TO A QUESTION
+app.post(`${prefix}/answer/add`, (req, res) => {
+  // Undetermined if values below will remain needed or not yet
+  // const { question_id, ...answerSub } = req.query;
+  res.sendStatus(201);
 });
-
-const port = 3001;
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`listening on port ${port}`);
 });
